f37a2b391a7f890a9729febcb1f345163fd25467
Jivesh Makkar <C0814268@mylambton.ca>
2021-09-19 16:59:23 -0400

    Initial Commit

---
+++ Lab_A1_A2_iOS_Jivesh_c0814268/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_A1_A2_iOS_Jivesh_c0814268
+//
+//  Created by user205607 on 9/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
